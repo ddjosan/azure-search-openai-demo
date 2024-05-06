1447b0b463b3ae180824094cffeaa4718ca295da
ddjosan <ddjosan@users.noreply.github.com>
2024-05-06 09:29:03 +0200

    adjust for UNDPRS

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,9 +3,9 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "How does UNDP Serbia contribute to the SDG Agenda?",
+    "List all UNDP Serbia projects directly related to dealing with climate change.",
+    "How do we accelerate the adoption of emerging technologies, such as artificial intelligence and biotech, in Serbia?"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -359,7 +359,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g.  how does UNDP Serbia contribute to the SDG Agenda?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
