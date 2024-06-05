4f05799e5d197827f1dd439ea44088bacf4db57b
UNDPCO\aleksandra.dimitrije <aleksandra.dimitrijevic@undp.org>
2024-06-05 11:25:53 +0200

    Change question input limit

--- app/frontend/src/components/QuestionInput/QuestionInput.tsx
+++ app/frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -44,7 +44,7 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend, init
     const onQuestionChange = (_ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
         if (!newValue) {
             setQuestion("");
-        } else if (newValue.length <= 1000) {
+        } else if (newValue.length <= 10000) {
             setQuestion(newValue);
         }
     };
